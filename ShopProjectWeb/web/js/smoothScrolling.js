95270174fa68e29c425129ad6a7293631e3a3c89
Maksim Samuilionak <maximwow70@gmail.com>
2017-02-20 22:24:29 +0300

    footer version 0.0.3

---
+++ ShopProjectWeb/web/js/smoothScrolling.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+	$(".btn-up").on("click","a", function (event) {
+		//отменяем стандартную обработку нажатия по ссылке
+		event.preventDefault();
+
+		//забираем идентификатор бока с атрибута href
+		var id  = $(this).attr('href'),
+
+		//узнаем высоту от начала страницы до блока на который ссылается якорь
+			top = $(id).offset().top;
+		
+		//анимируем переход на расстояние - top за 700 мс
+		$('body,html').animate({scrollTop: top}, 700);
+	});
+});
