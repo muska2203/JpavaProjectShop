ceee7b4935481f5383c22a0a3184f20866f5623a
Maksim Samuilionak <maximwow70@gmail.com>
2017-02-23 16:04:32 +0300

    shop-cart 0.0.2

--- ShopProjectWeb/web/js/shop-cart.js
+++ ShopProjectWeb/web/js/shop-cart.js
@@ -30,37 +30,24 @@ function newXMLHttpRequest() {
   return xmlreq;
 }
 function addToCart(itemId){
-    var req = new XMLHttpRequest();
-    // Оператор для получения сообщения обратного вызова 
-    // из объекта запроса
-    // Открываем HTTP-соединение с помощью POST-метода к 
-    //сервлету корзины покупателя.
-    // Третий параметр определяет, что запрос  асинхронный.
+    var req = new newXMLHttpRequest();
     req.open("POST", "TestServlet", true);
-    // Определяет, что в содержимом запроса есть данные 
     req.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-    // Посылаем закодированные данные, говорящие о том, что я хочу добавить 
-    // определенный продукт в корзину.
     req.send();
-            
-    req.onreadystatechange = function() { // (3)
-  if (req.readyState != 4) return;
-
-    console.log('Готово!');
-
-  if (req.status != 200) {
-    alert(req.status + ': ' + xhr.statusText);
-  } else {
-    alert(req.responseText+"||"+req.readyState);
-  }
-
-}
-    //var handlerFunction = getReadyStateHandler(req);
-    //req.onreadystatechange = handlerFunction;
-    //var q = req.responseText;
-    console.log(req.readyState+"|||"+req.responseText);
+    var str;
+    req.onreadystatechange = function() { 
+        if (req.readyState != 4) return;
+          console.log('Finish');
+        if (req.status != 200) {
+          console.log('Error!');
+        } else {
+          str = req.responseText;
+        }
+    }
+    return str;
 }
 
+/*
 function getReadyStateHandler(req) {
     // Возвращает неопределенную функцию, которая считывает 
     // данные XMLHttpRequest return function () {
@@ -74,10 +61,14 @@ function getReadyStateHandler(req) {
   } else {
     alert(req.responseText);
     }
-}
+}*/
+
+cartItems.innerHTML = 0;
+cartMoney.innerHTML = 0;
 for(var i = 0; i < btnAddCart.length; i++){
     btnAddCart[i].addEventListener("click", function(){
         $(this).addClass('clicked');
-        addToCart(i);
+        var value = addToCart(i);
+        cartMoney.innerHTML += value;
     });
 }
