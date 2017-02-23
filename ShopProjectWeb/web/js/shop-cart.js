0bed4e7296be3d9652bf48f7bb3dc089c474babd
Maksim Samuilionak <maximwow70@gmail.com>
2017-02-23 16:46:47 +0300

    shop-cart is working!!!!!!

--- ShopProjectWeb/web/js/shop-cart.js
+++ ShopProjectWeb/web/js/shop-cart.js
@@ -71,7 +71,7 @@ for(var i = 0; i < btnAddCart.length; i++){
     btnAddCart[i].addEventListener("click", function(){
         $(this).addClass('clicked');
         var req = new newXMLHttpRequest();
-        req.open("POST", "TestServlet", false);
+        req.open("POST", "TestServlet", true);
         req.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
         req.send();
         req.onreadystatechange = function() { 
@@ -80,8 +80,8 @@ for(var i = 0; i < btnAddCart.length; i++){
             if (req.status != 200) {
               console.log('Error!');
             } else {
-              str = req.responseText;
-              cartMoney.innerHTML += (+cartMoney.innerHTML) + (+str);
+              str = +req.responseText;
+              cartMoney.innerHTML = +(cartMoney.innerHTML) + (+str);
             }
         }
     });
