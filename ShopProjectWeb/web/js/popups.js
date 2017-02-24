510374369f450e99347a18313c131e75c6c98551
Maksim Samuilionak <maximwow70@gmail.com>
2017-02-25 00:12:05 +0300

    frontend 1.0.3

--- ShopProjectWeb/web/js/popups.js
+++ ShopProjectWeb/web/js/popups.js
@@ -1,6 +1,9 @@
 $('.btn-account').on("click", function(){
     $('.popup-account').addClass('open');
 });
+$('.btn-login').on('click', function(){
+    $('.popup-login').addClass('open');
+});
 $('.popup-close').on("click", function(){
     $('.popup-window-template').removeClass('open');
 });
--- ShopProjectWeb/web/js/smoothScrolling.js
+++ ShopProjectWeb/web/js/smoothScrolling.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-	$(".btn-up").on("click","a", function (event) {
+	$(".btn-up ").on("click","a", function (event) {
 		//отменяем стандартную обработку нажатия по ссылке
 		event.preventDefault();
 
@@ -9,6 +9,16 @@ $(document).ready(function(){
 		//узнаем высоту от начала страницы до блока на который ссылается якорь
 			top = $(id).offset().top;
 		
+		//анимируем переход на расстояние - top за 700 мс
+		$('body,html').animate({scrollTop: top}, 700);
+	});
+	$(".btn-shop-cart ").on("click", function (event) {
+		//отменяем стандартную обработку нажатия по ссылке
+		event.preventDefault();
+		//забираем идентификатор бока с атрибута href
+		var id  = $(this).attr('href'),
+		//узнаем высоту от начала страницы до блока на который ссылается якорь
+			top = $(id).offset().top;
 		//анимируем переход на расстояние - top за 700 мс
 		$('body,html').animate({scrollTop: top}, 700);
 	});
