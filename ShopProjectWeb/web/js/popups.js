bee27bc25475758e8f297e7b455ef148387aa16c
Maksim Samuilionak <maximwow70@gmail.com>
2017-02-26 13:32:17 +0300

    update popups

--- ShopProjectWeb/web/js/popups.js
+++ ShopProjectWeb/web/js/popups.js
@@ -1,8 +1,11 @@
 $('.btn-account').on("click", function(){
     $('.popup-account').addClass('open');
 });
-$('.btn-login').on('click', function(){
-    $('.popup-login').addClass('open');
+$('.btn-sign-in').on('click', function(){
+    $('.popup-sign-in').addClass('open');
+});
+$('.btn-sign-up').on('click', function(){
+    $('.popup-sign-up').addClass('open');
 });
 $('.popup-close').on("click", function(){
     $('.popup-window-template').removeClass('open');
