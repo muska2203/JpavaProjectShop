70d7d88d1bc99c388b5fd36c546033b13c515b09
Maksim Samuilionak <maximwow70@gmail.com>
2017-02-28 08:49:15 +0300

    encapsulate in smoothScrolling.js

--- ShopProjectWeb/web/js/smoothScrolling.js
+++ ShopProjectWeb/web/js/smoothScrolling.js
@@ -1,35 +1,20 @@
+//using jQuery
+var linksForSmoothScroll = [
+	'.btn-up',
+	'.btn-shop-cart',
+	'.nav a'
+];
 $(document).ready(function(){
-	$(".btn-up ").on("click","a", function (event) {
-		//отменяем стандартную обработку нажатия по ссылке
-		event.preventDefault();
-
-		//забираем идентификатор бока с атрибута href
-		var id  = $(this).attr('href'),
-
-		//узнаем высоту от начала страницы до блока на который ссылается якорь
-			top = $(id).offset().top;
-		
-		//анимируем переход на расстояние - top за 700 мс
-		$('body,html').animate({scrollTop: top}, 700);
-	});
-	$(".btn-shop-cart ").on("click", function (event) {
-		//отменяем стандартную обработку нажатия по ссылке
-		event.preventDefault();
-		//забираем идентификатор бока с атрибута href
-		var id  = $(this).attr('href'),
-		//узнаем высоту от начала страницы до блока на который ссылается якорь
-			top = $(id).offset().top;
-		//анимируем переход на расстояние - top за 700 мс
-		$('body,html').animate({scrollTop: top}, 700);
-	});
-	$(".nav a").on("click", function (event) {
-		//отменяем стандартную обработку нажатия по ссылке
-		event.preventDefault();
-		//забираем идентификатор бока с атрибута href
-		var id  = $(this).attr('href'),
-		//узнаем высоту от начала страницы до блока на который ссылается якорь
-			top = $(id).offset().top;
-		//анимируем переход на расстояние - top за 700 мс
-		$('body,html').animate({scrollTop: top}, 700);
-	});
+	for(var i = 0; i < linksForSmoothScroll.length; i++){
+		$(linksForSmoothScroll[i]).on("click", function (event) {
+			//отменяем стандартную обработку нажатия по ссылке
+			event.preventDefault();
+			//забираем идентификатор бока с атрибута href
+			var id  = $(this).attr('href'),
+			//узнаем высоту от начала страницы до блока на который ссылается якорь
+				top = $(id).offset().top;
+			//анимируем переход на расстояние - top за 700 мс
+			$('body,html').animate({scrollTop: top}, 700);
+		});
+	}
 });
